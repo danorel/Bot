1fac32357c83f512f6ca6c0d85862238a13a3e7e
danorel <mail.ordan@gmail.com>
2022-07-28 09:48:48 +0300

    Add StochasticBotCleanObservable implementation

--- src/main/java/StochasticBotCleanObservable.java
+++ src/main/java/StochasticBotCleanObservable.java
@@ -1,46 +1,67 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StochasticBotCleanObservable {
 
     private static final int N = 5;
 
-    private static boolean isDirtPoint(char[][] board, int posr, int posc) {
+    private static final boolean[][] visited = new boolean[N][N];
+
+    private static boolean isTargetPoint(char[][] board, int posr, int posc, char target) {
         if (posr < 0 || posr >= N || posc < 0 || posc >= N) {
             return false;
         }
-        return board[posr][posc] == 'd';
+        return board[posr][posc] == target;
+    }
+
+    private static void findInvisiblePoints(ArrayList<int[]> invisiblePoints, char[][] board, boolean[][] cache, int posr, int posc) {
+        if (posr < 0 || posr >= N || posc < 0 || posc >= N) {
+            return;
+        }
+        if (cache[posr][posc]) {
+            return;
+        }
+        cache[posr][posc] = true;
+        if (isTargetPoint(board, posr, posc, 'o')) {
+            invisiblePoints.add(new int[] { posr, posc });
+            return;
+        }
+        findInvisiblePoints(invisiblePoints, board, cache, posr - 1, posc);
+        findInvisiblePoints(invisiblePoints, board, cache, posr + 1, posc);
+        findInvisiblePoints(invisiblePoints, board, cache, posr, posc - 1);
+        findInvisiblePoints(invisiblePoints, board, cache, posr, posc + 1);
     }
 
     private static ArrayList<int[]> findDirtPoints(char[][] board, int posr, int posc) {
-        ArrayList<int[]> dirtPoints = new ArrayList<>();
+        ArrayList<int[]> targetPoints = new ArrayList<>();
 
         for (int i = -1; i <= 1; ++i) {
             for (int j = -1; j <= 1; ++j) {
-                if (isDirtPoint(board, posr + i, posc + j)) {
-                    dirtPoints.add(new int[] { posr + i, posc + j });
+                if (isTargetPoint(board, posr + i, posc + j, 'd')) {
+                    targetPoints.add(new int[] { posr + i, posc + j });
                 };
             }
         }
 
-        return dirtPoints;
+        return targetPoints;
     }
 
-    private static int[] findClosestDirtPoint(ArrayList<int[]> dirtPoints, int posr, int posc) {
-        int[] closestDirtPoint = new int[] { posr, posc };
+    private static int[] findClosestTargetPoint(ArrayList<int[]> targetPoints, int posr, int posc) {
+        int[] closestTargetPoint = new int[] { posr, posc };
 
-        int minDirtScore = Integer.MAX_VALUE;
+        int minTargetScore = Integer.MAX_VALUE;
 
-        for (int[] dirtPoint : dirtPoints) {
-            int dirtScore = findScore(posr, posc, dirtPoint[0], dirtPoint[1]);
-            if (dirtScore < minDirtScore) {
-                minDirtScore = dirtScore;
-                closestDirtPoint[0] = dirtPoint[0];
-                closestDirtPoint[1] = dirtPoint[1];
+        for (int[] targetPoint : targetPoints) {
+            int targetScore = findScore(posr, posc, targetPoint[0], targetPoint[1]);
+            if (targetScore < minTargetScore) {
+                minTargetScore = targetScore;
+                closestTargetPoint[0] = targetPoint[0];
+                closestTargetPoint[1] = targetPoint[1];
             }
         }
 
-        return closestDirtPoint;
+        return closestTargetPoint;
     }
 
     private static int findScore(int fromX, int fromY, int toX, int toY) {
@@ -50,10 +71,26 @@ public class StochasticBotCleanObservable {
     private static void nextMove(char[][] board, int posr, int posc) {
         ArrayList<int[]> dirtPoints = findDirtPoints(board, posr, posc);
 
-        int[] closestDirtPoint = findClosestDirtPoint(dirtPoints, posr, posc);
+        int dx, dy;
 
-        int dx = closestDirtPoint[0] - posr;
-        int dy = closestDirtPoint[1] - posc;
+        if (dirtPoints.size() == 0) {
+            ArrayList<int[]> invisiblePoints = new ArrayList<>();
+
+            boolean[][] cache = new boolean[N][N];
+            Arrays.stream(cache).forEach(row -> Arrays.fill(row, false));
+
+            findInvisiblePoints(invisiblePoints, board, cache, posr, posc);
+
+            int[] closestInvisiblePoint = findClosestTargetPoint(invisiblePoints, posr, posc);
+
+            dx = closestInvisiblePoint[0] - posr;
+            dy = closestInvisiblePoint[1] - posc;
+        } else {
+            int[] closestDirtPoint = findClosestTargetPoint(dirtPoints, posr, posc);
+
+            dx = closestDirtPoint[0] - posr;
+            dy = closestDirtPoint[1] - posc;
+        }
 
         if (dx < 0) {
             System.out.println("UP");
@@ -69,6 +106,7 @@ public class StochasticBotCleanObservable {
     }
 
     public static void main(String[] args) {
+
         /**
          * 0 0
          * b-ooo
@@ -89,7 +127,17 @@ public class StochasticBotCleanObservable {
          */
         String test2 = "1 0 --ooo bdooo --ooo ooooo ooooo";
 
-        Scanner scanner = new Scanner(test2);
+        /**
+         * 1 0
+         * --ooo
+         * b-ooo
+         * --ooo
+         * ooooo
+         * ooooo
+         */
+        String test3 = "1 0 --ooo boooo --ooo ooooo ooooo";
+
+        Scanner scanner = new Scanner(test3);
 
         int posr = scanner.nextInt();
         int posc = scanner.nextInt();
@@ -105,6 +153,8 @@ public class StochasticBotCleanObservable {
             }
             ++i;
         }
+
+        Arrays.stream(visited).forEach(row -> Arrays.fill(row, false));
 
         nextMove(board, posr, posc);
     }
